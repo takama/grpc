d7108e0c6cbd4ef6981b7eef06c2da63a77d95de
Igor Dolzhikov <bluesriverz@gmail.com>
2019-08-27 00:33:34 +0200

    Added client info command

---
+++ pkg/commands/info.go
@@ -0,0 +1,37 @@
+// Package commands contains global variables with configs and commands
+// nolint: gochecknoglobals, gochecknoinits
+package commands
+
+import (
+	"github.com/takama/grpc/pkg/boot"
+	"github.com/takama/grpc/pkg/client"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+// infoCmd represents the info command
+var infoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Get information about service",
+	Long: `This command provide service information via gRPC.
+Use --file or -f to specify JSON data file with batch requests`,
+	// nolint: unparam
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg, log := boot.Setup()
+		// nolint: errcheck
+		defer log.Sync()
+
+		// Runs the domain checker
+		if err := client.Info(
+			&cfg.Client, log,
+			boot.TLSOption(cfg.Client.Host, cfg.Client.Insecure),
+		); err != nil {
+			log.Fatal("Get info error", zap.Error(err))
+		}
+	},
+}
+
+func init() {
+	clientCmd.AddCommand(infoCmd)
+}
