61e84b897ce007e1b43cc34109b8339bad3fbf2b
Igor Dolzhikov <bluesriverz@gmail.com>
2019-08-26 23:02:32 +0200

    Added client config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/takama/grpc/pkg/client"
 	"github.com/takama/grpc/pkg/info"
 	"github.com/takama/grpc/pkg/logger"
 	"github.com/takama/grpc/pkg/server"
@@ -11,20 +12,24 @@ import (
 // Default values: host, port, etc
 const (
 	// ServiceName - default service name
-	ServiceName = "grpc"
+	ServiceName       = "grpc"
+	ClientServiceName = "grpc"
 
 	APIVersion = "v1"
 
 	DefaultConfigPath = "config/default.conf"
 
+	DefaultClientPort     = 8000
 	DefaultServerPort     = 8000
 	DefaultInfoPort       = 8080
+	DefaultClientInsecure = false
 	DefaultInfoStatistics = true
 	DefaultLoggerLevel    = logger.LevelInfo
 )
 
 // Config -- Base config structure
 type Config struct {
+	Client client.Config
 	Server server.Config
 	Info   info.Config
 	Logger logger.Config
@@ -33,6 +38,11 @@ type Config struct {
 // New - returns new config record initialized with default values
 func New() (*Config, error) {
 	cfg := &Config{
+		Client: client.Config{
+			Host:     ClientServiceName,
+			Port:     DefaultClientPort,
+			Insecure: DefaultClientInsecure,
+		},
 		Server: server.Config{
 			Port: DefaultServerPort,
 		},
