ac09bb8b34ed05a884a883bc272735ab7837a81e
Igor Dolzhikov <bluesriverz@gmail.com>
2019-08-27 00:33:51 +0200

    Added client ping command

---
+++ pkg/client/ping.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/takama/grpc/contracts/echo"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+// Ping command
+func Ping(cfg *Config, log *zap.Logger, message string, count int, opts ...grpc.DialOption) error {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port), opts...)
+	if err != nil {
+		return err
+	}
+	defer func(log *zap.Logger) {
+		if err := conn.Close(); err != nil {
+			log.Error("Connection close error:", zap.Error(err))
+		}
+	}(log)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	metadata := new(metadata.MD)
+
+	cl := echo.NewEchoClient(conn)
+	for idx := 1; idx <= count; idx++ {
+		response, err := cl.Ping(ctx, &echo.Request{
+			Content: fmt.Sprintf("%s: %d", message, idx),
+		}, grpc.Header(metadata))
+		if err != nil {
+			return err
+		}
+		log.Info(
+			"ping",
+			zap.String("message", response.Content),
+			zap.Any("hostname", metadata.Get("hostname")),
+		)
+	}
+
+	return nil
+}
---
+++ pkg/commands/ping.go
@@ -0,0 +1,49 @@
+// Package commands contains global variables with configs and commands
+// nolint: gochecknoglobals, gochecknoinits
+package commands
+
+import (
+	"strconv"
+
+	"github.com/takama/grpc/pkg/boot"
+	"github.com/takama/grpc/pkg/client"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+// pingCmd represents the ping command
+var pingCmd = &cobra.Command{
+	Use:   "ping",
+	Short: "Ping client/server connection",
+	Long:  `This command provide ping service via gRPC.`,
+	// nolint: unparam
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg, log := boot.Setup()
+		// nolint: errcheck
+		defer log.Sync()
+
+		// Read counts, initial value is 1
+		count := 1
+		countParam := cmd.Flag("count").Value.String()
+		if v, err := strconv.Atoi(countParam); err == nil {
+			count = v
+		}
+
+		// Runs the domain checker
+		if err := client.Ping(
+			&cfg.Client, log,
+			cmd.Flag("message").Value.String(), count,
+			boot.TLSOption(cfg.Client.Host, cfg.Client.Insecure),
+		); err != nil {
+			log.Fatal("Get info error", zap.Error(err))
+		}
+	},
+}
+
+func init() {
+	clientCmd.AddCommand(pingCmd)
+
+	pingCmd.PersistentFlags().String("message", "Hello", "Specify message to ping")
+	pingCmd.PersistentFlags().Int("count", 1, "Count of messages to ping")
+}
