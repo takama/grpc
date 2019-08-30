76d327936867249f38453011e163ca650acefcc7
Igor Dolzhikov <bluesriverz@gmail.com>
2019-08-30 14:06:07 +0200

    Moved dial option from boot package to connection package

--- pkg/boot/setup.go
+++ pkg/boot/setup.go
@@ -1,16 +1,11 @@
 package boot
 
 import (
-	"crypto/tls"
-	"time"
-
 	"github.com/takama/grpc/pkg/config"
 	"github.com/takama/grpc/pkg/helper"
 	"github.com/takama/grpc/pkg/logger"
 
 	"go.uber.org/zap"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
 // Setup the configuration, logger
@@ -21,27 +16,4 @@ func Setup() (*config.Config, *zap.Logger) {
 	log := logger.New(&cfg.Logger)
 
 	return cfg, log
-}
-
-// PrepareDialOptions gives options that manage TLS options,
-// retries and exponential backoff in calls to a service
-func PrepareDialOptions(
-	host string, insecure, waitForReady bool, maxDelay time.Duration,
-	opts ...grpc.DialOption,
-) []grpc.DialOption {
-	tlsOption := grpc.WithTransportCredentials(credentials.NewTLS(
-		&tls.Config{
-			ServerName: host,
-		},
-	))
-	if insecure {
-		tlsOption = grpc.WithInsecure()
-	}
-	return append(opts,
-		tlsOption,
-		grpc.WithDefaultCallOptions(
-			grpc.WaitForReady(waitForReady),
-		),
-		grpc.WithBackoffMaxDelay(maxDelay),
-	)
 }
